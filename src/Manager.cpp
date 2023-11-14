180c395212cf59862492c756419031d1b3b1e18a
Piotr Mikulowski <mikuspiotr123@gmail.com>
2023-11-14 16:02:25 +0100

    Move ManagerInterface method definitions into .cpp file, create &True, &False functions

--- src/Manager.cpp
+++ src/Manager.cpp
@@ -1,1 +1,25 @@
 #include "Manager.h"
+#include "ManagerInterface.h"
+
+
+namespace ClassProject {
+
+    ManagerInterface::ManagerInterface() {
+        ClassProject::BDD_ID_Entry entry = {"False", 0, 0, 0, 0};
+        Table.push_back(entry);
+
+        entry = {"True", 1, 1, 1, 1};
+        Table.push_back(entry);
+    }
+
+    const BDD_ID &ManagerInterface::False() {
+        static const BDD_ID ret = 0;
+        return ret;
+    }
+
+    const BDD_ID &ManagerInterface::True() {
+        static const BDD_ID ret = 1;
+        return ret;
+    }
+
+}
