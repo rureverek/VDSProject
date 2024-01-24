b4a536f913a5107bcec906d582ed2d03d94caf10
Piotr Mikulowski <mikuspiotr123@gmail.com>
2024-01-24 14:13:45 +0100

    add constructors, call createVar to create states, manage exception handling

--- src/reachability/Reachability.h
+++ src/reachability/Reachability.h
@@ -5,9 +5,12 @@
 
 namespace ClassProject {
 
-//    class Reachability : public ReachabilityInterface {
-//
-//    };
+    class Reachability : public ReachabilityInterface {
+    public:
+        Reachability() : ReachabilityInterface() {};
+        Reachability(unsigned int stateSize) : ReachabilityInterface(stateSize,0) {};
+        Reachability(unsigned int stateSize, unsigned int inputSize) : ReachabilityInterface(stateSize,inputSize) {};
+    };
 
 }
 #endif
