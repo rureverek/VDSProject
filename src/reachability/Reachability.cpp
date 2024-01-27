f937357b41e8d1a8be26e15b0487d74d1a8c7250
Piotr Mikulowski <mikuspiotr123@gmail.com>
2024-01-27 19:20:57 +0100

    fill constructor content

--- src/reachability/Reachability.cpp
+++ src/reachability/Reachability.cpp
@@ -15,8 +15,15 @@ using namespace ClassProject;
                 for(int i = 0; i < stateSize; i++)
                 {
                     std::string label = "s";
-                    label.append(std::to_string(i));
-                    Manager::createVar(label);
+                    label.append(std::to_string(i)); //Give label names: s0, s1, s2 ...
+                    BDD_ID id = Manager::createVar(label); //Create state variables
+                    current_states.push_back(id);
+                    initial_states.push_back(false); //Set initial state to false
+                    if(inputSize == 0) //Set next state to be current state if no input variables
+                    {
+                        next_states.push_back(id);
+                    }
+                    tran_func.push_back(Manager::True()); //Identity function by default
                 }
             }
             catch(std::exception const& e)
@@ -33,7 +40,7 @@ using namespace ClassProject;
     bool Reachability::isReachable(const std::vector<bool> &stateVector) {
         return false;
     }
-    void Reachability::setTransitionFunctions(const std::vector<BDD_ID> &transitionFunctions) {};
+    void Reachability::setTransitionFunctions(const std::vector<BDD_ID> &transitionFunctions) {}
     void Reachability::setInitState(const std::vector<bool> &stateVector) {}
     int Reachability::stateDistance(const std::vector<bool> &stateVector) {
     return 0;
