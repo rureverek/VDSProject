005c6391fb9c17ea9eb48e258009165cab91c679
Adam <adamghebrezgi@gmail.com>
2024-01-27 18:35:52 +0100

    added set InitState function

--- src/reachability/Reachability.cpp
+++ src/reachability/Reachability.cpp
@@ -38,4 +38,29 @@ using namespace ClassProject;
 
     bool Reachability::isReachable(const std::vector<bool> &stateVector) {};
     void Reachability::setTransitionFunctions(const std::vector<BDD_ID> &transitionFunctions) {};
-    void Reachability::setInitState(const std::vector<bool> &stateVector) {};
+
+/**
+    * Provides an initial state for the system as a vector of boolean values.
+    * If the entry is true, the state bit is high. Otherwise, the bit is low.
+    * E.g. initial state not(s0) and not(s1) is transformed into {false,false}.
+    *
+    * @param stateVector provides the assignment for each state bit
+    * @throws std::runtime_error if size does not match with number of state bits
+    */
+    void Reachability::setInitState(const std::vector<bool> &stateVector) {
+
+        if (stateVector.size() != stateSize)
+        {
+            throw std::runtime_error("Size of stateVector does not match the number of state bits.");
+        }
+
+        std::vector<int> intStateVector;
+
+        for (bool stateBit : stateVector)
+        {
+            intStateVector.push_back(stateBit ? 1 : 0);
+        }
+        // Set the currentState to the converted stateVector
+        initial_states = intStateVector;
+    };
+
