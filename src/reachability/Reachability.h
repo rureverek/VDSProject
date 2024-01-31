0390bf649f316295c530e1473f3f80741b9f4d37
Piotr Mikulowski <mikuspiotr123@gmail.com>
2024-01-31 12:29:12 +0100

    add missing variables

--- src/reachability/Reachability.h
+++ src/reachability/Reachability.h
@@ -22,10 +22,10 @@ namespace ClassProject {
 
     std::vector<BDD_ID> current_states;
     std::vector<BDD_ID> next_states;
-    std::vector<BDD_ID> tran_func;
-    std::vector<BDD_ID> tau;        //transition functions
+    BDD_ID tau;     //characteristic function of transition relation - tau
     std::vector<BDD_ID> inputs;     //fsm inputs
     std::vector<bool> initial_states;
+    BDD_ID c_s;     //characteristic function of the initial state - c_s
 
     };
 
